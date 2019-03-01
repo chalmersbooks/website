373a68467364f80dacf8148422f9a01352753ccc
lasanjin <sanjinslavnic@gmail.com>
2019-03-01 14:58:50 +0100

    added custom contraints for login and password page

---
+++ src/main/java/core/PasswordConstraints.java
@@ -0,0 +1,25 @@
+package core;
+
+public enum PasswordConstraints {
+    MIN("^(?=.{8,}).*$", "Password must contain at least 8 character"),
+    LOWERCASE("^(?=.*[a-z]).*$", "Password must contain at least one lower character"),
+    UPPERCASE("^(?=.*[A-Z]).*$", "Password must contain at least one uppercase character"),
+    NUMBER("^(?=.*[0-9]).*$", "Password must contain at least one number 0-9"),
+    SPECIAL("^(?=.*[@#$%^&+=]).*$", "Password must contain at least one special character: @#$%^&+=");
+
+    private String regex;
+    private String message;
+
+    PasswordConstraints(String regex, String message) {
+        this.regex = regex;
+        this.message = message;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/login/RegisterBean.java
+++ src/main/java/login/RegisterBean.java
@@ -1,10 +1,16 @@
 package login;
 
 import core.EmailTypes;
+import core.PasswordConstraints;
 import lombok.Getter;
 import lombok.Setter;
+import net.bootsfaces.utils.FacesMessages;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -17,8 +23,26 @@ public class RegisterBean implements Serializable {
     @Getter
     @Setter
     private Map<String, String> emails = new HashMap<>();
+
     @Getter
+    @Setter
     private String emailType;
+
+    @Getter
+    @Setter
+    /*
+    @Pattern(regexp = "^(?=.{8,}).*$", message = "Password must contain at least 8 character")
+    @Pattern(regexp = "^(?=.*[a-z]).*$", message = "Password must contain at least one lower character")
+    @Pattern(regexp = "^(?=.*[A-Z]).*$", message = "Password must contain at least one uppercase character")
+    @Pattern(regexp = "^(?=.*[0-9]).*$", message = "Password must contain at least one number 0-9")
+    @Pattern(regexp = "^(?=.*[@#$%^&+=]).*$", message = "Password must contain at least one special character: @#$%^&+=")
+    */
+    private String password;
+
+    @Getter
+    @Setter
+    private String confirmPassword;
+
 
     @PostConstruct
     public void setup() {
@@ -28,8 +52,36 @@ public class RegisterBean implements Serializable {
         }
     }
 
-    public void setEmailType(String emailType) {
-        System.out.println("PRINTAR EMAIL TYPEN: " + emailType);
-        this.emailType = emailType;
+    public void register() {
+        if (!verifiedPassword()) {
+            FacesMessages.error("Error!", "Password do not match");
+        } else {
+            FacesMessages.info("Info", "Success!");
+        }
     }
+
+    public void validatePassword(FacesContext context, UIComponent comp, Object value) {
+        PasswordConstraints[] pc = PasswordConstraints.values();
+        for (PasswordConstraints temp : pc) {
+            if (!isValid(value, temp.getRegex())) {
+                invalidPasswordMessage(context, comp, temp.getMessage());
+                break;
+            }
+        }
+    }
+
+    private void invalidPasswordMessage(FacesContext context, UIComponent comp, String message) {
+        //((UIInput) comp).setValid(false);
+        FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_WARN, message, null);
+        context.addMessage(comp.getClientId(context), fm);
+    }
+
+    private boolean isValid(Object password, String regex) {
+        return password.toString().matches(regex);
+    }
+
+    private boolean verifiedPassword() {
+        return password.equals(confirmPassword);
+    }
+
 }
